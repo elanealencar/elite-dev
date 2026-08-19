1a8b2906cbd87c67b5093a07088404baad59162a
elanealencar <elanealencar@gmail.com>
2026-08-18 23:49:37 -0300

    feat(api): initialize Express server

---
+++ apps/api/src/app.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express from "express";
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (_request, response) => {
+  return response.status(200).json({
+    status: "ok",
+  });
+});
